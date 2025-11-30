6d96117f02c4cf6a8e48b2206290b4ba260cdf5a
Nicholas Faleao <nickfaleao@gmail.com>
2025-11-30 23:45:53 +1000

    Completed State

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,26 @@
+import React from "react";
+import { useState } from "react";
+
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button
+        onClick={() => {
+          setValue((currentValue) => {
+            const newValue = currentValue + 1;
+            console.log("Current Value:", currentValue);
+            console.log("New Value:", newValue);
+            return newValue;
+          });
+        }}
+        className="btn"
+      >
+        Increase
+      </button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;
